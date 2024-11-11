4b3037c7ce5dbe0e02ed5ad0e0458bcad4526287
Adnanalways <adnanjoybi760@gmail.com>
2024-11-11 12:50:31 +0600

    new commit

--- XamproTest/src/test/java/xamprotest/Updateprofile.java
+++ XamproTest/src/test/java/xamprotest/Updateprofile.java
@@ -4,10 +4,11 @@ package xamprotest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Updateprofile {
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws InterruptedException  {
 		System.setProperty("webdriver.chrome.driver","C:\\\\selenium\\\\chromedriver-win64 (130)\\\\chromedriver-win64\\\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
@@ -22,6 +23,12 @@ public class Updateprofile {
 		driver.findElement(By.xpath("//div[@class=\"account-access-submit-button\"]")).click();
 		
 		//login successfully update profile 
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Update Profile")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@id='dob']")).sendKeys("06/25/1998");
+	
+		driver.findElement(By.xpath("//*[@id=\"radio-gender-male\"]")).click();
 	
 
 	}
